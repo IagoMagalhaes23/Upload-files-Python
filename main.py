f275e78e2d1e77196c801b4e3cc3848a9fafcf4b
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-06-21 21:19:38 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,37 +1,63 @@
-# -*- coding: utf-8 -*-
 import os
-from flask import Flask, request, jsonify, make_response
-from flask_restful import Resource, Api
-from json import dumps
+from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
-from flask_cors import CORS, cross_origin
-
-UPLOAD_FOLDER = 'folder'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+from flask import send_from_directory, jsonify
 
 app = Flask(__name__)
-CORS(app)
-api = Api(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+UPLOAD_FOLDER = './'
+
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv', 'xlsx'}
+
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-class UploadFiles(Resource):
-    def post(self):
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        
+        # verifique se a solicitacao de postagem tem a parte do arquivo
         if 'file' not in request.files:
-            return make_response(jsonify({"message":"Arquivo obrigatório!"}), 500) 
+            flash('Nao tem a parte do arquivo')
+            return redirect(request.url)
         file = request.files['file']
+        
+        # Se o usuario nao selecionar um arquivo, o navegador envia um
+        # arquivo vazio sem um nome de arquivo.
+        
+        if file.filename == '':
+            flash('Nenhum arquivo selecionado')
+            return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return make_response(jsonify({"message":"Upload realizado com sucesso!"}), 200) 
-
-api.add_resource(UploadFiles, '/') 
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <html>
+    <head>
+        <title>Upload File</title>
+    </head>
+    <body>
+        <h1>File Upload</h1>
+        <form method="POST" action="" enctype="multipart/form-data">
+        <p><input type="file" name="file"></p>
+        <p><input type="submit" value="Submit"></p>
+        </form>
+    </body>
+    </html>
+        '''
+    
+    
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug = True)
