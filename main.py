62f833e2208a734ed2bfc00b842847c416c493b8
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-06-21 22:13:39 -0300

    update files

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from werkzeug.utils import secure_filename
 from flask import send_from_directory, jsonify
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '1234'
 UPLOAD_FOLDER = './'
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv', 'xlsx'}
@@ -18,22 +17,23 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('Nao tem a parte do arquivo')
             return redirect(request.url)
+        
         file = request.files['file']
         
         if file.filename == '':
             flash('Nenhum arquivo selecionado')
             return redirect(request.url)
+        
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+            return render_template('index.html')
     return render_template('index.html')
     
 @app.route('/uploads/<name>')
